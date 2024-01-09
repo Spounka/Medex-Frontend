4be17bb53b465f74fbb92f9ffa0a180839ddbc04
Ahmed Fathalrhman Mahjoub <ahmedbelli080@gmail.com>
2024-01-09 14:07:45 +0300

    Added the new dashboard
    Signed-off-by: Ahmed Fathalrhman Mahjoub <ahmedbelli080@gmail.com>

---
+++ src/components/Supplier/Charts/IncomeAreaChart.jsx
@@ -0,0 +1,142 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+
+// material-ui
+import { useTheme } from "@mui/material/styles";
+
+// third-party
+import Chart from "react-apexcharts";
+
+// chart options
+const areaChartOptions = {
+  chart: {
+    height: 450,
+    type: "area",
+    toolbar: {
+      show: false,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+    width: 2,
+  },
+  grid: {
+    strokeDashArray: 0,
+  },
+};
+
+// ==============================|| INCOME AREA CHART ||============================== //
+
+const IncomeAreaChart = ({ slot }) => {
+  const theme = useTheme();
+
+  const { primary, secondary } = theme.palette.text;
+  const line = theme.palette.divider;
+
+  const [options, setOptions] = useState(areaChartOptions);
+
+  useEffect(() => {
+    setOptions((prevState) => ({
+      ...prevState,
+      colors: [theme.palette.primary.main, theme.palette.primary[700]],
+      xaxis: {
+        categories:
+          slot === "month"
+            ? [
+                "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "May",
+                "Jun",
+                "Jul",
+                "Aug",
+                "Sep",
+                "Oct",
+                "Nov",
+                "Dec",
+              ]
+            : ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        labels: {
+          style: {
+            colors: [
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+            ],
+          },
+        },
+        axisBorder: {
+          show: true,
+          color: line,
+        },
+        tickAmount: slot === "month" ? 11 : 7,
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: [secondary],
+          },
+        },
+      },
+      grid: {
+        borderColor: line,
+      },
+      tooltip: {
+        theme: "light",
+      },
+    }));
+  }, [primary, secondary, line, theme, slot]);
+
+  const [series, setSeries] = useState([
+    {
+      name: "Sales",
+      data: [0, 86, 28, 115, 48, 210, 136],
+    }
+  ]);
+
+  useEffect(() => {
+    setSeries([
+      {
+        name: "Sales",
+        data:
+          slot === "month"
+            ? [76, 85, 101, 98, 87, 105, 91, 114, 94, 86, 115, 35]
+            : [31, 40, 28, 51, 42, 109, 100],
+      }
+    ]);
+  }, [slot]);
+
+  return (
+    <Chart
+      options={options}
+      series={series}
+      type="area"
+      height={450}
+      style={{
+        backgroundColor: "white",
+        borderRadius: "15px",
+        border: "1px solid #cccccc",
+        boxShadow: "1px 3px 6px 0px #00000017"
+      }}
+    />
+  );
+};
+
+IncomeAreaChart.propTypes = {
+  slot: PropTypes.string,
+};
+
+export default IncomeAreaChart;
