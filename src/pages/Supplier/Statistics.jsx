37a824fc79eb89749e5e9a9c05dc100b63f125f4
Fadi Rezek Salloum <fadirezeksalloum.dev@gmail.com>
2024-01-14 12:33:58 +0100

    Added some statistics

--- src/pages/Supplier/Statistics.jsx
+++ src/pages/Supplier/Statistics.jsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 import useAxios from "../../utils/useAxios";
 
 import { IoStatsChart } from "react-icons/io5";
+import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
 
 const Statistics = () => {
     const { t } = useTranslation();
@@ -16,9 +17,14 @@ const Statistics = () => {
     const [customersCount, setCustomersCount] = useState(0);
 
     const [orderStatistics, setOrderStatistics] = useState({});
+    const [orderStatisticsPercentage, setOrderStatisticsPercentage] = useState(
+        {}
+    );
+
     const [quoteStatistics, setQuoteStatistics] = useState({});
 
     const [salesCount, setSalesCount] = useState({});
+    const [salesCountPercentage, setSalesCountPercentage] = useState({});
 
     const api = useAxios();
 
@@ -35,9 +41,16 @@ const Statistics = () => {
                 setCustomersCount(res?.data?.monthly_customers_count);
 
                 setOrderStatistics(res?.data?.monthly_orders_statistics);
+                setOrderStatisticsPercentage(
+                    res?.data?.monthly_orders_statistics_percentage
+                );
+
                 setQuoteStatistics(res?.data?.monthly_quotes_statistics);
 
                 setSalesCount(res?.data?.monthly_sales_count);
+                setSalesCountPercentage(
+                    res?.data?.monthly_sales_count_percentage
+                );
             });
     };
 
@@ -52,6 +65,142 @@ const Statistics = () => {
                     <IoStatsChart size="2rem" />
                     {t("buyer_pages.statistics.title")}
                 </h2>
+                <h4 className="fw-bold d-flex align-items-center gap-2 dashboard__title mt-5">
+                    {t("supplier_pages.statistics.monthly_orders_statistics")}
+                </h4>
+                <div className={`d-flex flex-wrap as`}>
+                    <div className="mt-3">
+                        <div className="p-4 dashboard__stats-card">
+                            <div className="row">
+                                <div className="col-9">
+                                    <div className="mb-4">
+                                        <h5 className="card-title mb-0 dashboard__stats-card-title">
+                                            {t(
+                                                "supplier_pages.statistics.orders_max"
+                                            )}
+                                        </h5>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-3">
+                                        <h2 className="d-flex align-items-center mb-0 dashboard__stats-card-text">
+                                            {orderStatistics?.highest_sale > 0
+                                                ? orderStatistics?.highest_sale
+                                                : 0}
+                                        </h2>
+                                        <div
+                                            className={`d-flex align-items-center gap-1 px-3 py-1 ${
+                                                orderStatisticsPercentage?.highest_sale >
+                                                0
+                                                    ? "gainColor"
+                                                    : "lossColor"
+                                            }`}
+                                        >
+                                            {Math.abs(
+                                                Math.round(
+                                                    orderStatisticsPercentage?.highest_sale
+                                                )
+                                            )}
+                                            %
+                                            {orderStatisticsPercentage?.highest_sale >
+                                            0 ? (
+                                                <FaArrowTrendUp size="1rem" />
+                                            ) : (
+                                                <FaArrowTrendDown size="1rem" />
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-3">
+                        <div className="p-4 dashboard__stats-card">
+                            <div className="row">
+                                <div className="col-9">
+                                    <div className="mb-4">
+                                        <h5 className="card-title mb-0 dashboard__stats-card-title">
+                                            {t(
+                                                "supplier_pages.statistics.orders_min"
+                                            )}
+                                        </h5>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-3">
+                                        <h2 className="d-flex align-items-center mb-0 dashboard__stats-card-text">
+                                            {orderStatistics?.lowest_sale > 0
+                                                ? orderStatistics?.lowest_sale
+                                                : 0}
+                                        </h2>
+                                        <div
+                                            className={`d-flex align-items-center gap-1 px-3 py-1 ${
+                                                orderStatisticsPercentage?.lowest_sale >
+                                                0
+                                                    ? "gainColor"
+                                                    : "lossColor"
+                                            }`}
+                                        >
+                                            {Math.abs(
+                                                Math.round(
+                                                    orderStatisticsPercentage?.lowest_sale
+                                                )
+                                            )}
+                                            %
+                                            {orderStatisticsPercentage?.lowest_sale >
+                                            0 ? (
+                                                <FaArrowTrendUp size="1rem" />
+                                            ) : (
+                                                <FaArrowTrendDown size="1rem" />
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-3">
+                        <div className="p-4 dashboard__stats-card">
+                            <div className="row">
+                                <div className="col-9">
+                                    <div className="mb-4">
+                                        <h5 className="card-title mb-0 dashboard__stats-card-title">
+                                            {t(
+                                                "supplier_pages.statistics.orders_avg"
+                                            )}
+                                        </h5>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-3">
+                                        <h2 className="d-flex align-items-center mb-0 dashboard__stats-card-text">
+                                            {orderStatistics?.average_sale > 0
+                                                ? orderStatistics?.average_sale.toFixed(
+                                                      2
+                                                  )
+                                                : 0}
+                                        </h2>
+                                        <div
+                                            className={`d-flex align-items-center gap-1 px-3 py-1 ${
+                                                orderStatisticsPercentage?.average_sale >
+                                                0
+                                                    ? "gainColor"
+                                                    : "lossColor"
+                                            }`}
+                                        >
+                                            {Math.abs(
+                                                Math.round(
+                                                    orderStatisticsPercentage?.average_sale
+                                                )
+                                            )}
+                                            %
+                                            {orderStatisticsPercentage?.average_sale >
+                                            0 ? (
+                                                <FaArrowTrendUp size="1rem" />
+                                            ) : (
+                                                <FaArrowTrendDown size="1rem" />
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     );
