d4b36b01cd50d27e3e4a858497e9d91e8ca77623
Ahmed Fathalrhman Mahjoub <ahmedbelli080@gmail.com>
2024-01-16 01:23:43 +0300

    test
    Signed-off-by: Ahmed Fathalrhman Mahjoub <ahmedbelli080@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./i18n.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode> <App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
