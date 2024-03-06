7cfe8a00dc2224bdcca6b8941c1e6c9ec52f1ef3
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-03-06 13:09:56 +0100

    Fixed vite types oversight

--- src/vite.end.d.ts
+++ src/vite.end.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string;
     readonly VITE_BACKEND_URL: string;
+    readonly VITE_BACKEND_WEBSOCKET_URL: string,
 }
 
 interface ImportMeta {
