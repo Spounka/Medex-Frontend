f9225b1041c28b6a14f0ce2206f9033b8b30db10
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-03-14 15:45:28 +0100

    refactored MultiStepForm.tsx
    Added types and cleaned up the code a little

--- src/hooks/useMultiStepForm.tsx
+++ src/hooks/useMultiStepForm.tsx
@@ -1,25 +1,38 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 
-const useMultiStepForm = (steps = []) => {
+interface StepsType {
+    form: ReactNode;
+    title: string;
+
+}
+
+interface MultiStepFormType {
+    currentStepIndex: number,
+    goTo: (index: number) => void,
+    next: () => void,
+    previous: () => void,
+    steps: StepsType[],
+    step: StepsType,
+    isFirstStep: boolean,
+    isLastStep: boolean,
+}
+
+const useMultiStepForm = (steps: StepsType[] = []): MultiStepFormType => {
     const [currentStepIndex, setCurrentStepIndex] = useState(0);
 
     const next = () => {
-        setCurrentStepIndex((i) => {
-            if (i >= steps.length - 1) return i;
-            return i + 1;
-        });
+        setCurrentStepIndex((currentIndex) => currentIndex >= steps.length - 1 ? currentIndex : currentIndex + 1);
     };
 
     const previous = () => {
-        setCurrentStepIndex((i) => {
-            if (i <= 0) return i;
-            return i - 1;
-        });
+        setCurrentStepIndex((currentIndex) => currentIndex <= 0 ? currentIndex : currentIndex - 1);
     };
 
-    const goTo = (index) => {
+    const goTo = (index: number) => {
         setCurrentStepIndex(index);
     };
+
+    const step = steps[currentStepIndex];
 
     return {
         currentStepIndex,
@@ -27,7 +40,7 @@ const useMultiStepForm = (steps = []) => {
         next,
         previous,
         steps,
-        step: steps[currentStepIndex],
+        step,
         isFirstStep: currentStepIndex === 0,
         isLastStep: currentStepIndex === steps.length - 1,
     };
