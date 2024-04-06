394e61da0c9a644a2d8f3d6ec50edd9f912cba0a
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-04-06 17:41:50 +0100

    fixed typo and added theme to tailwind
    added tailwind dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                purple: {
+                    DEFAULT: "#7f56da",
+                },
+                green: {
+                    default: "#1e6b49",
+                },
+                remy: {
+                    default: "#f5dcd4",
+                },
+                arsenic: {
+                    default: "#3a3e49",
+                },
+                grey: {
+                    storm: "#7a808c",
+                },
+            },
+        },
     },
+    prefix: 'tw-',
     plugins: [],
 };
