4cd4a7ff8c8701205b74fb929049bf7392535e36
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-04-06 17:34:12 +0100

    added tailwind dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./i18n.jsx";
 import { StrictMode } from "react";
+import "main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
