a6116116b7036985fee48eaac4d222dc5d665ed0
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-04-13 23:15:03 +0100

    Tailwind and font things

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./i18n.jsx";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
