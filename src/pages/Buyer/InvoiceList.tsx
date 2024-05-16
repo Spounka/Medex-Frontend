917a9961976de6b7d9ec614f130692177460f6a4
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-05-16 11:08:07 +0100

    Added api calls to the invoices pages
    Closes #8

--- src/pages/Buyer/InvoiceList.tsx
+++ src/pages/Buyer/InvoiceList.tsx
@@ -4,101 +4,81 @@ import { Link } from "react-router-dom";
 import { AgGridReact } from "ag-grid-react";
 import { AG_GRID_LOCALE_AR } from "../../utils/AG-Localization/ar";
 import { AG_GRID_LOCALE_EN } from "../../utils/AG-Localization/en";
+import axios from "axios";
+import { Invoice } from "@domain/invoice.ts";
+import { useQuery } from "@tanstack/react-query";
+import useAuthToken from "../../hooks/useAuthToken.tsx";
+import { ColDef } from "ag-grid-community";
 
-type TempInvoice = {
-    id: number;
-    client: string;
-    code: string;
-    date: string;
-    total: number;
-    status: string;
-};
+async function getInvoices(access: string) {
+    const result = await axios.get<Invoice[]>(
+        `${import.meta.env.VITE_BACKEND_URL}/api/quote/invoices/`,
+        {
+            headers: {
+                Authorization: `Bearer ${access}`,
+            },
+        },
+    );
+    return result.data;
+}
 
 function InvoiceList(props) {
     const { t, i18n } = useTranslation();
-    const rowData = [
-        // generate invoice data
+    const { authTokens } = useAuthToken();
+    const invoicesQuery = useQuery({
+        queryFn: async () => getInvoices(authTokens.access),
+        queryKey: ["invoices"],
+        enabled: !!authTokens.access,
+    });
+
+    const columnDefs: ColDef[] = [
         {
-            id: 1,
-            client: "Client 01",
-            code: "INV-0001",
-            date: "12/12/2021",
-            total: 250,
-            status: "Paid",
-        },
-        {
-            id: 2,
-            client: "Client 01",
-            code: "INV-0002",
-            date: "03/12/2021",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 3,
-            client: "Client 01",
-            code: "INV-0003",
-            date: "08/12/2021",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 4,
-            client: "Client 01",
-            code: "INV-0004",
-            date: "05/10/2021",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 5,
-            client: "Client 01",
-            code: "INV-0005",
-            date: "01/12/2022",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 6,
-            client: "Client 01",
-            code: "INV-0006",
-            date: "11/12/2023",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 7,
-            client: "Client 01",
-            code: "INV-0007",
-            date: "12/12/2023",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 8,
-            client: "Client 01",
-            code: "INV-0008",
-            date: "12/12/2024",
-            total: 250,
-            status: "Cancelle",
-        },
-    ];
-    const columnDefs = [
-        {
-            field: "code",
-            headerName: "Code",
+            field: "invoice_id",
+            headerName: "Invoice",
             minWidth: 110,
         },
         {
-            field: "client",
-            headerName: "Client",
+            field: "user",
+            headerName: "Supplier",
             minWidth: 120,
+            cellRenderer: (params) => {
+                return <p>{params.data.user.full_name}</p>;
+            },
         },
         {
-            field: "date",
+            field: "user",
+            headerName: "Email",
+            minWidth: 120,
+            cellRenderer: (params) => {
+                return <p>{params.data.user.email}</p>;
+            },
+        },
+        {
+            field: "created",
             headerName: "Date",
             minWidth: 120,
             flex: 1,
+            cellRenderer: (params) => {
+                return <p>{new Date(params.data.created).toLocaleDateString()}</p>;
+            },
+        },
+        {
+            field: "delivery_date",
+            headerName: "Delivery Date",
+            minWidth: 120,
+            flex: 1,
+            cellRenderer: (params) => {
+                return <p>{new Date(params.data.delivery_date).toLocaleDateString()}</p>;
+            },
+        },
+        {
+            field: "payment_type",
+            headerName: "Payment Type",
+            minWidth: 120,
+            flex: 1,
+            cellRenderer: (params) => {
+                return <p>{params.data.payment_type}</p>;
+            },
         },
         {
             field: "total",
@@ -106,6 +86,9 @@ function InvoiceList(props) {
             resizable: true,
             minWidth: 150,
             flex: 1,
+            cellRenderer: (params) => {
+                return <p>1000 {t("sar")}</p>;
+            },
         },
         {
             field: "status",
@@ -113,16 +96,16 @@ function InvoiceList(props) {
             cellRenderer: (params) => {
                 return (
                     <p className={"tw-flex tw-items-center tw-gap-1 tw-text-start "}>
-                        {params.data.status === "Paid" && (
+                        {params.data.status === "A" && (
                             <span className="tw-h-2 tw-w-2 tw-rounded-full tw-bg-green"></span>
                         )}
-                        {params.data.status === "Pending" && (
+                        {params.data.status === "P" && (
                             <span className="tw-h-2 tw-w-2 tw-rounded-full tw-bg-orange-400"></span>
                         )}
-                        {params.data.status === "Cancelled" && (
+                        {params.data.status === "D" && (
                             <span className="tw-h-2 tw-w-2 tw-rounded-full tw-bg-red-700"></span>
                         )}
-                        {params.data.status}
+                        {params.data.status_display}
                     </p>
                 );
             },
@@ -157,7 +140,7 @@ function InvoiceList(props) {
                     style={{ height: 500 }}
                 >
                     <AgGridReact
-                        rowData={rowData}
+                        rowData={invoicesQuery.data}
                         columnDefs={columnDefs}
                         localeText={
                             i18n.language === "ar" ? AG_GRID_LOCALE_AR : AG_GRID_LOCALE_EN
--- src/pages/Supplier/InvoiceList.tsx
+++ src/pages/Supplier/InvoiceList.tsx
@@ -5,92 +5,81 @@ import { AgGridReact } from "ag-grid-react";
 import { AG_GRID_LOCALE_AR } from "../../utils/AG-Localization/ar";
 import { AG_GRID_LOCALE_EN } from "../../utils/AG-Localization/en";
 import Container from "../../components/ui/container";
+import axios from "axios";
+import { Invoice } from "@domain/invoice.ts";
+import useAuthToken from "../../hooks/useAuthToken.tsx";
+import { useQuery } from "@tanstack/react-query";
+import { ColDef } from "ag-grid-community";
+
+async function getInvoices(access: string) {
+    const result = await axios.get<Invoice[]>(
+        `${import.meta.env.VITE_BACKEND_URL}/api/quote/invoices/`,
+        {
+            headers: {
+                Authorization: `Bearer ${access}`,
+            },
+        },
+    );
+    return result.data;
+}
 
 function InvoiceList(props) {
     const { t, i18n } = useTranslation();
-    const rowData = [
-        // generate invoice data
+    const { authTokens } = useAuthToken();
+    const invoicesQuery = useQuery({
+        queryFn: async () => getInvoices(authTokens.access),
+        queryKey: ["invoices"],
+        enabled: !!authTokens.access,
+    });
+
+    const columnDefs: ColDef[] = [
         {
-            id: 1,
-            client: "Client 01",
-            code: "INV-0001",
-            date: "12/12/2021",
-            total: 250,
-            status: "Paid",
-        },
-        {
-            id: 2,
-            client: "Client 01",
-            code: "INV-0002",
-            date: "03/12/2021",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 3,
-            client: "Client 01",
-            code: "INV-0003",
-            date: "08/12/2021",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 4,
-            client: "Client 01",
-            code: "INV-0004",
-            date: "05/10/2021",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 5,
-            client: "Client 01",
-            code: "INV-0005",
-            date: "01/12/2022",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 6,
-            client: "Client 01",
-            code: "INV-0006",
-            date: "11/12/2023",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 7,
-            client: "Client 01",
-            code: "INV-0007",
-            date: "12/12/2023",
-            total: 250,
-            status: "Pending",
-        },
-        {
-            id: 8,
-            client: "Client 01",
-            code: "INV-0008",
-            date: "12/12/2024",
-            total: 250,
-            status: "Cancelled",
-        },
-    ];
-    const columnDefs = [
-        {
-            field: "code",
-            headerName: "Code",
+            field: "invoice_id",
+            headerName: "Invoice",
             minWidth: 110,
         },
         {
-            field: "client",
-            headerName: "Client",
+            field: "user",
+            headerName: "Supplier",
             minWidth: 120,
+            cellRenderer: (params) => {
+                return <p>{params.data.user.full_name}</p>;
+            },
         },
         {
-            field: "date",
+            field: "user",
+            headerName: "Email",
+            minWidth: 120,
+            cellRenderer: (params) => {
+                return <p>{params.data.user.email}</p>;
+            },
+        },
+        {
+            field: "created",
             headerName: "Date",
             minWidth: 120,
             flex: 1,
+            cellRenderer: (params) => {
+                return <p>{new Date(params.data.created).toLocaleDateString()}</p>;
+            },
+        },
+        {
+            field: "delivery_date",
+            headerName: "Delivery Date",
+            minWidth: 120,
+            flex: 1,
+            cellRenderer: (params) => {
+                return <p>{new Date(params.data.delivery_date).toLocaleDateString()}</p>;
+            },
+        },
+        {
+            field: "payment_type",
+            headerName: "Payment Type",
+            minWidth: 120,
+            flex: 1,
+            cellRenderer: (params) => {
+                return <p>{params.data.payment_type}</p>;
+            },
         },
         {
             field: "total",
@@ -98,6 +87,9 @@ function InvoiceList(props) {
             resizable: true,
             minWidth: 150,
             flex: 1,
+            cellRenderer: (params) => {
+                return <p>1000 {t("sar")}</p>;
+            },
         },
         {
             field: "status",
@@ -105,16 +97,16 @@ function InvoiceList(props) {
             cellRenderer: (params) => {
                 return (
                     <p className={"tw-flex tw-items-center tw-gap-1 tw-text-start "}>
-                        {params.data.status === "Paid" && (
+                        {params.data.status === "A" && (
                             <span className="tw-h-2 tw-w-2 tw-rounded-full tw-bg-green"></span>
                         )}
-                        {params.data.status === "Pending" && (
+                        {params.data.status === "P" && (
                             <span className="tw-h-2 tw-w-2 tw-rounded-full tw-bg-orange-400"></span>
                         )}
-                        {params.data.status === "Cancelled" && (
+                        {params.data.status === "D" && (
                             <span className="tw-h-2 tw-w-2 tw-rounded-full tw-bg-red-700"></span>
                         )}
-                        {params.data.status}
+                        {params.data.status_display}
                     </p>
                 );
             },
@@ -129,7 +121,7 @@ function InvoiceList(props) {
                         className={
                             "tw-max-h-fit tw-w-full tw-bg-white tw-px-2 tw-py-0 tw-text-center tw-text-black"
                         }
-                        to={`/supplier/invoices/${params.data.id}`}
+                        to={`/account/dashboard/invoices/${params.data.id}`}
                     >
                         View Details
                     </Link>
@@ -153,7 +145,7 @@ function InvoiceList(props) {
                     style={{ height: 500 }}
                 >
                     <AgGridReact
-                        rowData={rowData}
+                        rowData={invoicesQuery.data}
                         columnDefs={columnDefs}
                         localeText={
                             i18n.language === "ar" ? AG_GRID_LOCALE_AR : AG_GRID_LOCALE_EN
